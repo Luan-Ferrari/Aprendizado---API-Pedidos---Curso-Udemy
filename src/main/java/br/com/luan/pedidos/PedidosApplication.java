4f1ce42fa3e62e9de9b936ea6f36d759c332954a
Luan Ferrari <luanferrari.22@gmail.com>
2021-03-22 06:29:59 -0300

    Teste organizacao pacotes

--- src/main/java/br/com/luan/pedidos/PedidosApplication.java
+++ src/main/java/br/com/luan/pedidos/PedidosApplication.java
@@ -2,12 +2,20 @@ package br.com.luan.pedidos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PedidosApplication {
+public class PedidosApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PedidosApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PedidosApplication.class);
 	}
 
 }
---
+++ src/main/java/br/com/luan/pedidos/resources/CategoriaResource.java
@@ -0,0 +1,4 @@
+package br.com.luan.pedidos.resources;
+
+public class CategoriaResource {
+}
